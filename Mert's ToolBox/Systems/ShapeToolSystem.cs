5380e648bad043d9f709e5898f92dfa96e81c1e7
Flashbond <mrt_nr@hotmail.com>
2026-05-15 12:25:41 +0300

    v2.6.1
    * Elevation fix for Perfect Shape Tool

--- Mert's ToolBox/Systems/ShapeToolSystem.cs
+++ Mert's ToolBox/Systems/ShapeToolSystem.cs
@@ -244,10 +244,7 @@ namespace MertsToolBox.Systems
             if (m_CurrentSessionDimension < minAllowed)
                 m_CurrentSessionDimension = minAllowed;
 
-            subNets = null;
-            widthCells = 0;
-            depthCells = 0;
-            costElevation = 0f;
+            subNets = null; widthCells = depthCells = 0; costElevation = 0f;
 
             float inputD = m_CurrentSessionDimension - m_CurrentRoadWidth;
             if (inputD <= 0f)
